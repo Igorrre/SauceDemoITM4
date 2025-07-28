37413416d8aa47e8b533a67d43d3d51826f7148e
Igor <igorrefx@gmail.com>
2025-07-28 18:52:14 +0300

    Fix push

--- src/test/java/tests/BaseTest.java
+++ src/test/java/tests/BaseTest.java
@@ -4,6 +4,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.edge.EdgeOptions;
 import org.testng.ITestResult;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
@@ -44,7 +45,9 @@ public class BaseTest {
             options.addArguments("--headless");
             driver = new ChromeDriver(options);
         } else if (browser.equalsIgnoreCase("edge")) {
-            driver = new EdgeDriver();
+            EdgeOptions options = new EdgeOptions();
+            options.addArguments("--headless");
+            driver = new EdgeDriver(options);
         }
         softAssert = new SoftAssert();
 
@@ -63,6 +66,5 @@ public class BaseTest {
         if (driver != null) {
             driver.quit();
         }
-//        driver.quit();
     }
 }
